6ed6b25880e641be8345a3e1568ef881de61e44d
Richard <rtlloyd@gmail.com>
2023-05-21 18:17:38 -0400

    third commit

---
+++ FibonacciProject/src/Fibonacci.java
@@ -0,0 +1,21 @@
+/**
+ * Software Development II CEN-4025C *Assignment 2 *Eclipse ide Version: 2023-03
+ * *Build id: 20230309-1520
+ * 
+ * @author Richard
+ **/
+public class Fibonacci {
+    public static int fibonacci(int n) {
+	if (n <= 1) {
+	    return n;
+	} else {
+	    return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+    }
+
+    public static void main(String[] args) {
+	int n = 10;
+	int result = fibonacci(n);
+	System.out.println("The " + n + "th term of the Fibonacci sequence is " + result + ".");
+    }
+}
