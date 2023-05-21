f3e9b008feb84a8460f7329ccc2fc1cef9eac9cc
Richard <rtlloyd@gmail.com>
2023-05-21 19:15:00 -0400

    Added Javadoc

--- FibonacciProject/src/Fibonacci.java
+++ FibonacciProject/src/Fibonacci.java
@@ -4,6 +4,13 @@
  * 
  * @author RTL
  **/
+/**
+ * This method calculates the nth term of the Fibonacci sequence recursively.
+ *
+ * @param n The position in the Fibonacci sequence to calculate. Should be a
+ *          non-negative integer.
+ * @return The nth term of the Fibonacci sequence.
+ */
 public class Fibonacci {
     public static int fibonacci(int n) {
 	if (n <= 1) {
@@ -13,6 +20,16 @@ public class Fibonacci {
 	}
     }
 
+    /**
+     * This is the main method which serves as the entry point for the program.
+     * 
+     * It calls the fibonacci method with 10 as an argument and prints out the 10th
+     * term of the Fibonacci sequence in the following format: "The nth term of the
+     * Fibonacci sequence is y." where n is the term number and y is the value of
+     * that term in the Fibonacci sequence.
+     * 
+     * @param args An array of command-line arguments for the application
+     */
     public static void main(String[] args) {
 	int n = 10;
 	int result = fibonacci(n);
